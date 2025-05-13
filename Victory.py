33957bbff561b9f750b37cf91dde5f4950f2e3ae
rosecjc <r90235@gmail.com>
2025-05-13 11:16:43 +0800

    Update Victory.py

--- Victory.py
+++ Victory.py
@@ -71,6 +71,8 @@ for code in custom_codes:
     row = {
         "股票代號": code,
         "股票名稱": name,
+        "產業類別": twstock.codes[code].industry if code in twstock.codes else "-",
+        "股本（億）": twstock.codes[code].capital if code in twstock.codes else "-",
         "今日收盤價": price if price else "-",
         "外資買賣超": inst["外資買賣超"] if inst else "-",
         "投信買賣超": inst["投信買賣超"] if inst else "-",
